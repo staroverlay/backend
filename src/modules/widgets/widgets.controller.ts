339fde020516f3d5238a23f12f1be3ae56508b68
Sammwy <sammwy.dev@gmail.com>
2023-09-03 05:37:46 -0300

    feature: better rendering

--- src/modules/widgets/widgets.controller.ts
+++ src/modules/widgets/widgets.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 
 import { WidgetsService } from './widgets.service';
+import { Template } from '../templates/models/template';
 
 @Controller('/widgets')
 export class WidgetsController {
@@ -14,6 +15,14 @@ export class WidgetsController {
       throw new NotFoundException('Widget not found');
     }
 
-    return widget;
+    const template: Template = JSON.parse(widget.templateRaw);
+
+    return {
+      enabled: widget.enabled,
+      html: template.html,
+      settings: JSON.parse(widget.settings || '{}'),
+      scopes: widget.scopes || [],
+      userId: widget.userId,
+    };
   }
 }
