bcfbe76f58f9c3831c5c2658863265d79d4399d2
Sammwy <sammwy.dev@gmail.com>
2026-04-23 09:01:51 -0300

    chore: added drizzle config files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
-import { env } from "./src/lib/env";
-
 export default defineConfig({
     schema: "./src/database/schema.ts",
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
-        url: env.DATABASE_URL,
+        url: process.env.DATABASE_URL!,
     },
 });
